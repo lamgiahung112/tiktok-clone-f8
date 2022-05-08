1b8b20f1045d8b8919a17c60e0e2e3df0a8e6059
Hung Lam <hung.lam@redharegames.com>
2022-05-08 14:45:42 +0700

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+// Layouts
+import { HeaderOnly } from "~/components/Layouts"
+
+import Home from "~/pages/Home"
+import Following from "~/pages/Following"
+import Upload from "~/pages/Upload"
+
+// PUBLIC ROUTES
+export const publicRoutes = [
+    { path: "/", component: Home },
+    { path: "/following", component: Following },
+    { path: "/upload", component: Upload, layout: HeaderOnly },
+]
+
+// PRIVATE ROUTES
+export const privateRoutes = []

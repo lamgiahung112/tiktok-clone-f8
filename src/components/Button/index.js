92a71272308688237131680162bcb8feb93051c6
Hung Lam <hung.lam@redharegames.com>
2022-06-02 22:17:10 +0700

    Header UI #3

---
+++ src/components/Button/index.js
@@ -0,0 +1,55 @@
+import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
+
+import styles from './Button.module.css'
+
+const cx = classNames.bind(styles)
+
+function Button({ to, href, 
+                    primary = false, outline = false, text = false, rounded = false,
+                    small = false, large = false,
+                    disabled = false,
+                    className,
+                    onClick, children, ...passProps }) {
+    let Comp = 'button'
+    const _props = {
+        onClick,
+        ...passProps
+    }
+
+    // remove event listeners when disabled
+    if (disabled) {
+        Object.keys(_props).forEach(key => {
+            if (key.startsWith('on') && typeof _props[key] === 'function') {
+                delete _props[key]
+            }
+        })
+    }
+
+    if (to) {
+        _props.to = to
+        Comp = Link
+    }
+    else if (href) {
+        _props.href = href
+        Comp = 'a'
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        text,
+        rounded,
+        small,
+        large,
+        disabled,
+    })
+
+
+    return <Comp className={classes} {..._props}>
+        <span>{children}</span>
+    </Comp>;
+}
+
+export default Button;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -12,6 +12,7 @@ import styles from "./Header.module.css"
 import classNames from "classnames/bind"
 import images from "~/assets/img"
 import AccountItem from "~/components/AccountItem"
+import Button from "~/components/Button"
 
 const cx = classNames.bind(styles)
 
@@ -62,7 +63,10 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx("actions")}></div>
+                <div className={cx("actions")}>
+                    <Button text >Upload</Button>
+                    <Button primary >Log in</Button>
+                </div>
             </div>
         </header>
     )
